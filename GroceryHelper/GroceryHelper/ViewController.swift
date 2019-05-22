df79f4b87418ba16b5cc5cd6b25517a292fd07e0
emni4510 <user150278@103-246-99-68.moboware.com>
2019-05-22 11:19:29 +1000

    prototype

---
+++ GroceryHelper/GroceryHelper/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  GroceryHelper
+//
+//  Created by user150278 on 5/18/19.
+//  Copyright © 2019 user150278. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var signupView: UIView!
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    
+    
+    @IBAction func segmentedControlClicked(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            signupView.isHidden = true
+            loginView.isHidden = false
+            print("login")
+            break;
+        case 1:
+            signupView.isHidden = false
+            loginView.isHidden = true
+            print("sign up")
+            break;
+        default:
+            break;
+        }
+    }
+    
+    @IBAction func logIn(_ sender: Any) {
+        
+    }
+}
+
